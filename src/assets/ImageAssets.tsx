2147986ee08e876537e5750c2d174bffd59edbc2
CodeSprint01 <codesprintsite@gmail.com>
2024-06-14 00:45:53 +0500

    Logo added

--- src/assets/ImageAssets.tsx
+++ src/assets/ImageAssets.tsx
@@ -1,5 +1,7 @@
 export const ImageAssets = {
   Icons: { ArrowRight },
+  Navbar: { Logo: DrcodersLogo, alt: "Drcoders Logo" },
 };
 
 import { ArrowRight } from "lucide-react";
+import DrcodersLogo from "@/assets/Images/DrcodersLogo.png";
--- src/components/NavbarAndFooterComponents/Navbar.tsx
+++ src/components/NavbarAndFooterComponents/Navbar.tsx
@@ -2,12 +2,22 @@ import MaxContainer from "../MaxContainer";
 import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
 import NavLinks from "./NavLinks";
+import Image from "next/image";
+import { ImageAssets } from "@/assets/ImageAssets";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="py-7">
       <MaxContainer className="flex items-center justify-between">
-        <div>Logo</div>
+        <Link href={"/"}>
+          <Image
+            src={ImageAssets.Navbar.Logo}
+            alt={ImageAssets.Navbar.alt}
+            priority
+            className="h-14 w-auto"
+          />
+        </Link>
 
         <div className="flex items-center gap-10">
           <NavLinks />
