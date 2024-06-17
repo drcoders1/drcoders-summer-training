09861c7a2e75e072696f7fe7226ba068304329f8
developer-256 <ch256.it@gmail.com>
2024-06-17 23:47:52 +0500

    added vision n mission and offerings component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import Offerings from "@/components/HomeComponents/Offerings";
 import SpotlightPreview from "@/components/HomeComponents/SpotlightPreview";
+import VisionMission from "@/components/HomeComponents/VisionMission";
 import React from "react";
 
 const page = () => {
   return (
     <main>
       <SpotlightPreview />
+      <VisionMission />
+      <Offerings />
     </main>
   );
 };
---
+++ src/components/HomeComponents/Offerings.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import MaxContainer from "../MaxContainer";
+
+const Offerings = () => {
+  return (
+    <>
+      <h1 className="mb-10 mt-60 text-center text-4xl font-semibold uppercase text-white">
+        What We are Offering?
+      </h1>
+
+      <MaxContainer className="flex flex-col gap-5">
+        <div className="flex items-center justify-center gap-9">
+          <p className="inline rounded-full bg-base-yellow px-14 py-4 text-black shadow-md">
+            Lorem ipsum dolor
+          </p>
+
+          <p className="inline rounded-full bg-base-orange px-14 py-4 text-black shadow-md">
+            Lorem ipsum dolor
+          </p>
+          <p className="inline rounded-full bg-base-yellow px-14 py-4 text-black shadow-md">
+            Lorem ipsum dolor
+          </p>
+        </div>
+
+        <div className="flex items-center justify-center gap-9">
+          <p className="inline rounded-full bg-base-purple px-14 py-4 text-black shadow-md">
+            Lorem ipsum dolor
+          </p>
+
+          <p className="inline rounded-full bg-base-light-blue px-14 py-4 text-black shadow-md">
+            Lorem ipsum dolor
+          </p>
+          <p className="inline rounded-full bg-base-purple px-14 py-4 text-black shadow-md">
+            Lorem ipsum dolor
+          </p>
+        </div>
+      </MaxContainer>
+    </>
+  );
+};
+
+export default Offerings;
